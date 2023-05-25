c0bb94c8e758731e3af890961645bf2de69fb190
Xekerr <corentin.pimentel@gmail.com>
2023-05-25 19:08:07 +0200

    ADD:deal timestampable

--- Economax/config/bundles.php
+++ Economax/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
