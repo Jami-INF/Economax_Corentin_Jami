1e746a0f7a04b767de8cbd5d6148a4436b714bb4
Jami infante <jami@MacBook.local>
2023-06-13 09:40:18 +0200

    ADD : Entity marchand

---
+++ Economax/src/Repository/MarchandRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Marchand;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Marchand>
+ *
+ * @method Marchand|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Marchand|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Marchand[]    findAll()
+ * @method Marchand[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MarchandRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Marchand::class);
+    }
+
+    public function save(Marchand $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Marchand $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Marchand[] Returns an array of Marchand objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('m.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Marchand
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
