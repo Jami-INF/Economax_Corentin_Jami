d652f0374d0ad515669a67f16e139e0fd4275a81
Jami infante <jami@MacBook.lan>
2023-06-15 09:38:16 +0200

    UPD : merge req comments

--- Economax/src/Repository/MarchandRepository.php
+++ Economax/src/Repository/MarchandRepository.php
@@ -21,7 +21,7 @@ class MarchandRepository extends ServiceEntityRepository
         parent::__construct($registry, Marchand::class);
     }
 
-    public function save(Marchand $entity, bool $flush = false): void
+    public function save(Marchand $entity, bool $flush = true): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class MarchandRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Marchand $entity, bool $flush = false): void
+    public function remove(Marchand $entity, bool $flush = true): void
     {
         $this->getEntityManager()->remove($entity);
 
